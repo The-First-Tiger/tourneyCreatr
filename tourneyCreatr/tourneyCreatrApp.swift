d6df406fdd46aae0ca4000d3f38393b9000793a3
TheTommynator <max.enders@gmail.com>
2021-12-28 10:22:14 +0100

    Initial Commit

---
+++ tourneyCreatr/tourneyCreatrApp.swift
@@ -0,0 +1,17 @@
+//
+//  tourneyCreatrApp.swift
+//  tourneyCreatr
+//
+//  Created by Maximilian Enders on 28.12.21.
+//
+
+import SwiftUI
+
+@main
+struct tourneyCreatrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
